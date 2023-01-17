3e2fadd691365bd6af554215b9544710d31184df
Thibaut COSENZA <113136288+Thibautcsnz@users.noreply.github.com>
2023-01-17 19:34:58 +0100

    Add files via upload

---
+++ Exercice 2.py
@@ -0,0 +1,5 @@
+age = int(input("Donnez votre age : \n"))
+
+annee = 2022 - age
+
+print(f"Votre annee de naissance est : {annee}")
---
+++ Exercice 8.py
@@ -0,0 +1,4 @@
+x = float(input("Entrez un nombre décimal: "))
+if x >=2 and x<3 or x>0 and x<=1 or x>=-10 and x<=-2:
+    print(f"{x} appartient à I")
+else: print(f"{x} n'appartient pas à I")
